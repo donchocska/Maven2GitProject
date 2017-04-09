ded1b77b7c0a78af547fd0bab7d0dc8614f1f5fd
donchocska <DONY@DESKTOP-0AA08HN>
2017-04-09 11:48:50 +0300

    След новия branch

--- maven2/src/main/java/com/fmi/maven/maven2/App.java
+++ maven2/src/main/java/com/fmi/maven/maven2/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-       new MySimpleClass().mySimpleMethod(null);;
+       new MySimpleClass().mySimpleMethod(null);
        
     }
 }
