ca46f62e9b8fd949510736078901ce7fee0c6a7b
donchocska <DONY@DESKTOP-0AA08HN>
2017-04-09 10:42:14 +0300

    Промених Класа App.java за да добавя инстанция на класа MySimpleClass

--- maven2/src/main/java/com/fmi/maven/maven2/App.java
+++ maven2/src/main/java/com/fmi/maven/maven2/App.java
@@ -1,5 +1,7 @@
 package com.fmi.maven.maven2;
 
+import com.fmi.maven.maven1.MySimpleClass;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new MySimpleClass().mySimpleMethod("Иван");
     }
 }
