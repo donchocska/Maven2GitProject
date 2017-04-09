b2c41d5e8a245e620dbdb4c8f55e7c93cb7a8393
donchocska <DONY@DESKTOP-0AA08HN>
2017-04-09 10:34:56 +0300

    Create new project

---
+++ maven2/src/main/java/com/fmi/maven/maven2/App.java
@@ -0,0 +1,13 @@
+package com.fmi.maven.maven2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
